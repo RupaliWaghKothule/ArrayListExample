bf314fd35aeea7c56ee9bb2dc554e3a6c4d761fc
rupaliwagh <rupaliwagh2016@gmail.com>
2023-01-08 15:08:44 +0530

    This is ArrayListExample Code

---
+++ src/com/velocity/ArrayListExample.java
@@ -0,0 +1,21 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		
+		
+		List<String> list =new ArrayList<String>();
+		list.add("Rupali");
+		list.add("Tushar");
+		list.add("Kavita");
+		list.add("Abhishek");
+		for(String s:list) {
+			
+			System.out.println();
+		}
+	}
+
+}
