bf61bf23cfcc5787c8c0a6c5ab541135ed60d63d
rupaliwagh <rupaliwagh2016@gmail.com>
2023-01-08 15:30:55 +0530

    final changes added

---
+++ src/com/velocity/LinkedListDemo.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+public static void main(String[] args) {
+	List<Integer> list=new LinkedList<Integer>();
+	list.add(1);
+	list.add(3);
+	System.out.println(list);
+} 
+
+}
